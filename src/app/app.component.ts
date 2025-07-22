31b577fa894aeaafd5bd3b132b48ea77fe65898c
Adrian Blas <militho.blazitho@gmail.com>
2025-07-22 12:50:12 -0600

    End of Section 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ import { HomeComponent } from './home/home.component';
   //imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [NavComponent, HomeComponent]
+  imports: [RouterOutlet, NavComponent, HomeComponent]
 })
 export class AppComponent implements OnInit {
   private accountService = inject(AccountService);
